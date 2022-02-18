7570a985ba535f87d10bd107c0ece5dae533e461
bli23-01 <hust_hjj@126.com>
2022-02-18 23:16:34 +0800

    Update map.py

--- map.py
+++ map.py
@@ -1,8 +1,7 @@
-import code
 import math
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.animation import FuncAnimation
+from matplotlib import cm
 
 class Road:
     def __init__(self, start_point, end_point):
@@ -21,13 +20,13 @@ class Road:
 class Rider:
     def __init__(self, gender = True):
         self.gender = gender
-        self.velocity = 2
+        self.velocity = 10
         self.cur_road = 0 #the index of roads
         self.cur_point = roads[self.cur_road].start_point
         self.cur_dist = 0
     def dist(self,target):
         return np.sum((self.cur_point - target)**2, axis=1, keepdims=True)**0.5
-    def update(self, rate = 10):    # 1s
+    def update(self, rate = 1):    # 1s
         ramain_dis = self.velocity * rate
         while self.cur_dist + ramain_dis > roads[self.cur_road].road_length:
             ramain_dis -= roads[self.cur_road].road_length
@@ -41,37 +40,48 @@ class Rider:
 def update(i):
     #ax.scatter3D(coods[:, 0],coods[:, 1], coods[:, 2], cmap='Blues')  #绘制散点图
     #plt.clear()
-    #ax.plot3D(coods[:, 0],coods[:, 1], coods[:, 2], 'gray')
+    plt.cla()
+    ax.set_xlim([-3000,2000])
+    ax.set_ylim([-4000,1000])
+    ax.set_zlim([0,1000])
+    ax.plot3D(coods[:, 0],coods[:, 1], coods[:, 2], 'gray')
     riders_points = []
     for rider in riders:
         riders_points.append(rider.update())
     riders_points = np.array(riders_points)
     ax.scatter3D(riders_points[:, 0], riders_points[:, 1], riders_points[:, 2], 'red')
+    surf = ax.plot_surface(curve[:, 0], curve[:, 1], curve[:, 2], cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    fig.colorbar(surf, shrink=0.5, aspect=5)
 
 def get_data():
     f = open("data.csv", "r")
     s = f.read()
     coodStrs = s.split(" ")
     coods = []
+    curve = []
     zero_cood = list(map(float,coodStrs[0].split(",")))
     for s1 in coodStrs:
         t = list(map(float,s1.split(",")))
         t[0] = (t[0] - zero_cood[0]) / 0.00001141
         t[1] = (t[1] - zero_cood[1]) / 0.00000899
         coods.append(t)
+        curve.append(t)
+        curve.append([t[0], t[1], 0])
     f.close()
-    return np.array(coods)
+    return np.array(coods), np.array(curve)
         
 if __name__ == '__main__':
 
-    coods = get_data()
+    coods, curve = get_data()
+    print(np.shape(curve))
 
     global roads
     roads = []
+    
 
     for i in range(0, len(coods)):
         roads.append(Road(coods[i], coods[(i + 1) % len(coods)]))
-        print(roads[i].road_length)
+        #print(roads[i].road_length)
 
     coods = np.array(coods)
     fig = plt.figure()
@@ -87,8 +97,10 @@ if __name__ == '__main__':
 
     ax.plot3D(coods[:, 0],coods[:, 1], coods[:, 2], 'gray')
     cur_time = 0
-    while cur_time < 10000:
-        update(0)
-        plt.pause(0.01)
+    surf = ax.plot_surface(curve[:, 0], curve[:, 1], curve[:, 2], cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    #while cur_time < 10000:
+    #    update(0)
+    #    plt.pause(0.001)
 #    anim = FuncAnimation(fig, update, frames=np.arange(0, 10), interval=1)
     plt.show()
